3af3c88acfa332400c3637833c2a356a4ee8277b
Ansoncool333 <1060196411@qq.com>
2014-11-15 23:55:42 +0800

    entity and dao building

---
+++ wechat/src/main/java/cn/anson/wechat/dao/MybatisDao.java
@@ -0,0 +1,25 @@
+package cn.anson.wechat.dao;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MybatisDao {
+	private static SqlSessionFactory sqlSessionFactory;
+	private static Reader reader; 
+	
+	static{
+	    try{
+	        reader    = Resources.getResourceAsReader("Configuration.xml");
+	        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		}catch(Exception e){
+		    e.printStackTrace();
+		}
+	}
+	
+	public static SqlSessionFactory getSession(){
+	    return sqlSessionFactory;
+	}
+}
--- wechat/src/main/java/cn/anson/wechat/entity/User.java
+++ wechat/src/main/java/cn/anson/wechat/entity/User.java
@@ -2,31 +2,14 @@ package cn.anson.wechat.entity;
 
 import java.util.Date;
 
-import javax.persistence.Id;
-
-import org.hibernate.annotations.Entity;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
-
-import cn.anson.wechat.annotation.Display;
-import cn.anson.wechat.annotation.Label;
-
-@Label("用户表")
 public class User {
 
-	@Id
-    @Label("用户ID")
     long userId;
 
-    @Label("用户名")
-    @Length(min=2, max=45)
-    @NotEmpty
     String name;
 
-    @Label("创建时间")
     Date createAt;
     
-    @Label("最后登录时间")
     Date loginAt;
 
 	public long getUserId() {
@@ -61,7 +44,6 @@ public class User {
 		this.loginAt = loginAt;
 	}
 
-	@Display
     public String display() {
         StringBuilder buf = new StringBuilder();
         buf.append(userId).append('-').append(name);
